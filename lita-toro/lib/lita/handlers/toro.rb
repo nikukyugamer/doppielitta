515f84e71c0a46e21cde7ccd085d39367ee84573
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-07-25 18:05:26 +0900

    fix: 🐛 連続してレスポンスを取得できなかった不具合を（無理矢理）修正

--- lita-toro/lib/lita/handlers/toro.rb
+++ lita-toro/lib/lita/handlers/toro.rb
@@ -7,16 +7,25 @@ module Lita
     class Toro < Handler
       RANDOM_CAT_API_URL = 'https://aws.random.cat/meow'.freeze
 
-      route /^ねこ画像.*\Z/, :cat_image, help: { 'ねこ画像' => 'ねこ画像を表示します' }
+      route(/\Aねこ画像.*\Z/, :cat_image, help: { '\Aねこ画像.*\Z' => 'ねこ画像を表示します' })
 
       def cat_image(response)
         uri = URI(RANDOM_CAT_API_URL)
         res = Net::HTTP.get_response(uri)
 
-        return if res.code != '200'
+        if res.code != '200'
+          response.reply('見つかりませんでしたニャ……')
+
+          return
+        end
 
         res_hash = JSON.parse(res.body)
-        response.reply(res_hash['file'])
+        reply_text = res_hash['file']
+
+        response.reply(reply_text) unless reply_text.nil?
+
+        # なぜか一度リクエストを挟まないと次が受け付けられないようなので、入れる
+        Net::HTTP.get_response(uri)
       end
 
       Lita.register_handler(self)

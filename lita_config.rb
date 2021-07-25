ab60585a3c1c627d7a1a19b9f3bb0a47b7ee1c3b
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-07-25 14:30:36 +0900

    feat: 🎸 Heroku で眠らせないように /healthcheck のエンドポイントを作った (#23)

--- lita_config.rb
+++ lita_config.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Style/FrozenStringLiteralComment, Style/AsciiComments
+# rubocop:disable Style/FrozenStringLiteralComment, Style/AsciiComments, Style/ConditionalAssignment
 Lita.configure do |config|
   # The name your robot will use.
   config.robot.name = 'Lita'
@@ -36,12 +36,15 @@ Lita.configure do |config|
   ## Example: Set options for the Redis connection.
   # config.redis.host = ENV['REDIS_URL']
   # config.redis.port = 1234
-  # 下の二つの環境変数の名前は Heroku での予約語である
-  config.redis[:url] = ENV['REDISTOGO_URL']
+
+  # REDISTOGO_URL は Redis To Go における事実上の予約語である
+  ENV['REDIS_URL'].nil? ? config.redis[:url] = ENV['REDISTOGO_URL'] : config.redis[:url] = ENV['REDIS_URL']
+
+  # PORT は Heroku における予約語である
   config.http.port = ENV['PORT']
 
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
   # config.handlers.some_handler.some_config_key = "value"
 end
-# rubocop:enable Style/FrozenStringLiteralComment, Style/AsciiComments
+# rubocop:enable Style/FrozenStringLiteralComment, Style/AsciiComments, Style/ConditionalAssignment
